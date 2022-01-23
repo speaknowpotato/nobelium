3c588ddbc954d9bad7ddd89a27d5d983fb1e2dbd
bricyma <brightmzb@gmail.com>
2022-01-22 23:21:27 -0800

    update config

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'oldmaru',
   email: 'brightmzb@gmail.com',
   link: 'https://oldmaru.vercel.app/',
-  description: '.',
+  description: 'MLOps, AWS, Kubeflow, Machine learning',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://www.linkedin.com/in/zhibei-ma-30385bb8/',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
